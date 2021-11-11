ea9cb288a20a44b98ae59a69adfd62b6daa11395
revut <revutskyg@gmail.com>
2021-11-11 10:00:33 +0200

    global update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galeryRef = document.querySelector(".gallery");
+const markUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join("");
+
+galeryRef.insertAdjacentHTML("beforeend", markUp);
+let instance = null;
+galeryRef.addEventListener("click", onGallery);
+function onGallery(e) {
+  e.preventDefault();
+  if (e.target.tagName !== "IMG") {
+    return;
+  }
+  const preview = e.target.dataset.source;
+  instance = basicLightbox.create(`
+    <img src="${preview}" width="800" height="600">
+`);
+  instance.show();
+  // console.log(e.target.dataset.source);
+}
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    instance?.close();
+  }
+});
 
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const galeryRef = document.querySelector(".gallery");
+const markUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      // data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join("");
+galeryRef.insertAdjacentHTML("beforeend", markUp);
+// // const gallaryClick = document.querySelector(".gallery__image");
+// galeryRef.addEventListener("click", onGallery);
+// function onGallery(e) {
+//   e.preventDefault();
+//   if (e.target.tagName !== "IMG") {
+//     return;
+//   }
+// }
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
 console.log(galleryItems);
